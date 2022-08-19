2831b4e0b86355bdeba3275f6147ff9c360ccecc
Sergio chaplin <Sergiorabota133@gmail.com>
2022-08-19 06:58:31 +0300

    solutions of quadratic equations, mortgage payment calculator

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,39 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+   let arr = [];
+   let d = (b ** 2 - 4 * a * c);
+   let xOne = (-b + Math.sqrt(d)) / (2 * a);
+   let xTwo = (-b - Math.sqrt(d)) / (2 * a);
+   if (a === 0) {
+      return `Зачение a = 0, не допустимо!`;
+   } else if (d > 0) {
+      arr.push(xOne);
+      arr.push(xTwo);
+   } else if (d === 0) {
+      arr.push(-b / (2 * a));
+   }
+   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+function calculateTotalMortgage(percent = 0, contribution = 0, amount, date) {
+   let totalAmount;
+   let actualDate = new Date();
+   let P = percent / 100 / 12;
+   let n = (date.getFullYear() - actualDate.getFullYear()) * 12 + date.getMonth() - actualDate.getMonth();
+   let meaningСontribution = parseInt(contribution);
+   let meaningAmount = parseInt(amount);
+   let S = +amount - +contribution;
+   if (Number.isNaN(P)) {
+      return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+   } else if (Number.isNaN(meaningСontribution)) {
+      return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+   } else if (Number.isNaN(meaningAmount)) {
+      return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+   }
+   let totalPayment = S * P * (1 + 1 / (Math.pow(1 + P, n) - 1));
+   let totalAmountRound = totalPayment * n;
+   totalAmount = Math.round(totalAmountRound * 100) / 100;
+   return totalAmount;
 }
