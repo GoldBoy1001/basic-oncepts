cbbf09392b6a9bab74c6e43cceb67e0fb5bc9d8b
Sergio chaplin <Sergiorabota133@gmail.com>
2022-08-27 13:57:19 +0300

    homeworks-objeckts

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+   this.name = name;
+   this.gender = gender;
+   this.age = age;
 }
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+   this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+
+Student.prototype.addMark = function (mark) {
+   if (this.marks === undefined) {
+      this.marks = [mark];
+   }
+   else {
+      this.marks.push(mark);
+   }
+}
+
+Student.prototype.addMarks = function (...grade) {
+   if (this.marks === undefined) {
+      this.marks = [...grade];
+   }
+   else {
+      this.marks.push(...grade);
+   }
+}
+
+Student.prototype.getAverage = function () {
+   let sum = 0;
+   this.marks.forEach((mark) => sum += parseInt(mark));
+   return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+   delete this.subject;
+   delete this.marks;
+   this.excluded = reason;
+}
