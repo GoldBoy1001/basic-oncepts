6e1395825c6e0651060724ec5e1154f397f05ce8
Sergio chaplin <Sergiorabota133@gmail.com>
2022-09-16 12:13:14 +0300

    homeworks-decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,41 @@
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+   let cache = [];
+   function wrapper(...args) {
+      const hash = args.join(',');
+      let objectInCache = cache.find((item) => item.hash === hash);
+      if (objectInCache) {
+         console.log("Из кэша: " + objectInCache.result);
+         return "Из кэша: " + objectInCache.result;
+      }
+
+      let result = func(...args);
+      cache.push({ hash, result });
+      if (cache.length > 5) {
+         cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+   }
+   return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+   let timeoutId;
+   wrapper.count = 0;
+   wrapper.allCount = 0;
+   function wrapper(...args) {
+      if (!timeoutId) {
+         wrapper.count++;
+         func(...args);
+      }
+      wrapper.allCount++;
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+         wrapper.count++;
+         return func(...args);
+      }, delay)
+   }
+   return wrapper;
 }
+
